71e0822d998574d059fee12392e04e9a5d77e10c
Bruno Ch <135856797+0WhitherGgr0@users.noreply.github.com>
2024-06-22 18:53:14 -0500

    update

---
+++ src/main/java/com/mycompany/proyectoada/ProyectoADA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectoada;
+
+/**
+ *
+ * @author ROBERTO-SN850
+ */
+public class ProyectoADA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
